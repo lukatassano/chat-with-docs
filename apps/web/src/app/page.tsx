3a17f5114703464249c5f422e33c4fdc3fe20df6
Luka Tassano <lukatassano070@gmail.com>
2024-02-15 23:30:12 -0300

    feat(web/app/page): add home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,23 +1,32 @@
-"use client";
-
-import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import ParticlesBackground from "@ui/components/particles/ParticlesBackground";
+import { Button } from "@ui/components/ui/button";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
-  const { status } = useSession();
-  const { push } = useRouter();
-
-  useEffect(() => {
-    login();
-  }, [status]);
-
-  function login() {
-    if (status === "loading") return;
-
-    // eslint-disable-next-line no-unused-expressions
-    status === "authenticated" ? push("chat") : signIn("keycloak");
-  }
-
-  return <></>;
+  return (
+    <ParticlesBackground>
+      <div className="h-screen flex flex-col items-center justify-center gap-6">
+        <div className="text-center font-bold">
+          <h1 className="col-start-1 row-start-2 mt-4 max-w-[36rem] text-4xl font-extrabold tracking-tight sm:text-7xl xl:max-w-[43.5rem]">
+            Converse com seus documentos
+          </h1>
+        </div>
+        <div className="max-w-xl text-center font-light">
+          <p className="text-zinc-300">
+            de forma inteligente e eficiente. O futuro da produtividade está
+            aqui.
+            <br></br>
+            (a IA criou isso 😰)
+          </p>
+        </div>
+        <Link href="/chat">
+          <Button className="mt-4">
+            Ir para o chat
+            <ChevronRight className="ml-2 h-4 w-4" />
+          </Button>
+        </Link>
+      </div>
+    </ParticlesBackground>
+  );
 }
